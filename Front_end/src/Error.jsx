f01c0175947008569bba022137f4e4aeab697337
Uttam Kumar Sahoo <sahoouttamkumar26@gmail.com>
2025-04-21 17:54:36 +0530

    Delete Front_end directory

--- Front_end/src/Error.jsx
+++
@@ -1,8 +0,0 @@
-const Error = ()=>{
-    return(
-        <>
-            <h2>Auth Fail : 404</h2>
-        </>
-    )
-}
-export default Error;
--- Front_end/src/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import "./Spinner.css";
-
-const Spinner = () => (
-  <div className="spinner-overlay">
-    <div className="spinner"></div>
-  </div>
-);
-
-export default Spinner;
